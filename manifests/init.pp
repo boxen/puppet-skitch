cee2eee70e2c5d2b5976a9e0b1d6ddb119073990
Zac Sprackett <zac@sprackett.com>
2014-07-17 20:36:34 -0400

    Update skitch to 2.7.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class skitch {
   package { 'Skitch':
     provider => 'compressed_app',
-    source   => 'http://cdn1.evernote.com/skitch/mac/release/Skitch-2.6.1.zip'
+    source   => 'http://cdn1.evernote.com/skitch/mac/release/Skitch-2.7.2.zip'
   }
 }
